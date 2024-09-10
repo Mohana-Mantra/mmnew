d080c0696ba35e6a5d60ab9021d295242dbac651
koushikamboru <koushic1980@gmail.com>
2024-09-11 00:47:56 +0530

    old nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,16 +10,15 @@ function Navbar() {
 
     return (
         <>
-            {/* Desktop Navbar */}
-            <header className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 hidden md:block w-auto max-w-[80%] px-6 py-4 rounded-[45px] font-sulpher font-bold text-[18px] bg-[rgba(255,255,255,0.1)] backdrop-blur-[4px] border border-[rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] transition-all duration-500 ease-in-out hover:bg-[rgba(255,255,255,0.2)] hover:backdrop-blur-[6px] hover:shadow-2xl">
+            <header className="fixed rounded-b-[50px] top-0 inset-x-0 z-50 hidden md:block w-full h-20 md:px-20 font-sulpher font-bold text-[18px]">
                 <nav className="w-full h-full flex items-center justify-evenly">
-                    <div className="h-full flex items-center justify-center gap-8">
-                        <Link href="/" className={`${path === "/" && "text-[#feca00]"}`}>
+                    <div className="h-full flex items-center justify-center gap-12">
+                        <Link href="/" className={` ${path === "/" && "text-[#feca00]"}`}>
                             Home
                         </Link>
                         <Link
                             href="/events"
-                            className={`${path.includes("/events") && "text-[#feca00]"}`}
+                            className={` ${path.includes("/events") && "text-[#feca00]"}`}
                         >
                             Events
                         </Link>
@@ -31,18 +30,37 @@ function Navbar() {
                         </Link>
                         <Link
                             href="/contact"
-                            className={`${path.includes("/contact") && "text-[#feca00]"}`}
+                            className={` ${path.includes("/contact") && "text-[#feca00]"}`}
                         >
                             Contact
                         </Link>
                     </div>
 
-                    <div className="flex items-center h-full gap-8">
+                    <div className="flex items-center h-full gap-12">
+                        {/* <Link
+                            href="/register"
+                            className={`border-[2px] border-secondary py-2 px-4 ${
+                                path === "/register" && "text-[#feca00]"
+                            }`}
+                            onClick={() => (window.location.pathname = "/register")}
+                        >
+                            Register
+                        </Link>
+                        <Link
+                            href="/login"
+                            className={`bg-secondary text-black px-4 py-2 border-[2px] border-secondary ${
+                                path === "/login" && "text-[#feca00]"
+                            }`}
+                            onClick={() => window.location.reload()}
+                        >
+                            Login
+                        </Link> */}
                         <Link
                             href="https://mmregistration.vercel.app/"
                             className={`bg-secondary text-black px-4 py-2 border-[2px] border-secondary ${
                                 path === "/login" && "text-[#feca00]"
-                            } transition-all duration-300 ease-in-out hover:bg-black hover:text-white`}
+                            }`}
+                            // onClick={() => window.location.reload()}
                         >
                             Buy Your Pass
                         </Link>
@@ -50,11 +68,14 @@ function Navbar() {
                 </nav>
             </header>
 
-            {/* Mobile Hamburger Menu */}
             <div
-                onClick={() => setHamburgerOpen((prev) => !prev)}
+                onClick={() =>
+                    setHamburgerOpen((prev) => {
+                        return !prev;
+                    })
+                }
                 className={cn(
-                    "m-4 p-3 bg-[rgba(255,255,255,0.1)] backdrop-blur-[4px] border border-[rgba(255,255,255,0.18)] rounded-md w-10 aspect-square fixed z-[110] cursor-pointer top-0 left-0 md:hidden flex flex-col justify-between items-start transition-all ease-in-out duration-150",
+                    "m-4 p-3 border-white bg-black/10 backdrop-blur-3xl border rounded-md w-10 aspect-square fixed z-[110] cursor-pointer top-0 left-0 md:hidden flex flex-col justify-between items-start transition-all ease-in-out duration-150",
                     {
                         "items-center justify-center": isHamburgerOpen,
                     }
@@ -83,10 +104,9 @@ function Navbar() {
                 />
             </div>
 
-            {/* Mobile Menu */}
             <section
                 className={cn(
-                    "fixed z-[100] pl-6 pb-8 pr-10 bg-[rgba(255,255,255,0.1)] backdrop-blur-[4px] opacity-0 rounded-[45px] pt-20 border border-[rgba(255,255,255,0.18)] flex flex-col gap-6 -translate-x-[101%] transition-all ease-in-out duration-700",
+                    "fixed z-[100] pl-6 pb-8 pr-10 bg-white/5 backdrop-blur-3xl opacity-0 rounded-[14px] pt-20 border border-white flex flex-col gap-6 -translate-x-[101%] transition-all ease-in-out duration-700",
                     {
                         "m-2 translate-x-0 opacity-100": isHamburgerOpen,
                     }
@@ -94,42 +114,42 @@ function Navbar() {
             >
                 <Link
                     href="/"
-                    className={`${path === "/" && "text-[#feca00]"}`}
+                    className={` ${path === "/" && "text-[#feca00]"}`}
                     onClick={() => setHamburgerOpen(false)}
                 >
                     Home
                 </Link>
                 <Link
                     href="/events"
-                    className={`${path.includes("/events") && "text-yellow-400"}`}
+                    className={` ${path.includes("/events") && "text-yellow-400"}`}
                     onClick={() => setHamburgerOpen(false)}
                 >
                     Events
                 </Link>
                 <Link
                     href="/gallery"
-                    className={`${path.includes("/gallary") && "text-yellow-400"}`}
+                    className={` ${path.includes("/gallary") && "text-yellow-400"}`}
                     onClick={() => setHamburgerOpen(false)}
                 >
                     Gallery
                 </Link>
                 <Link
                     href="/contact"
-                    className={`${path.includes("/contact") && "text-yellow-400"}`}
+                    className={` ${path.includes("/contact") && "text-yellow-400"}`}
                     onClick={() => setHamburgerOpen(false)}
                 >
                     Contact
                 </Link>
                 <Link
                     href="/register"
-                    className={`${path.includes("/register") && "text-yellow-400"}`}
+                    className={` ${path.includes("/register") && "text-yellow-400"}`}
                     onClick={() => setHamburgerOpen(false)}
                 >
                     Register
                 </Link>
                 <Link
                     href="/login"
-                    className={`${path.includes("/login") && "text-yellow-400"}`}
+                    className={` ${path.includes("/login") && "text-yellow-400"}`}
                     onClick={() => setHamburgerOpen(false)}
                 >
                     Login
