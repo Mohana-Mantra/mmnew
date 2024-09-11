6277902ffa852ca4927a17fcf875058beb1696c1
koushikamboru <koushic1980@gmail.com>
2024-09-11 10:28:41 +0530

    next

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    // output: "export",
     trailingSlash: true,
     images: {
-        unoptimized: true,
+        // unoptimized: true,
         remotePatterns: [
             {
                 hostname: "**",
