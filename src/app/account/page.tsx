72ea82065386661cc4185257fab24b1b273e235e
Sairam Kagitha <sairamkagitha5@gmail.com>
2024-09-17 10:00:34 +0530

    sess

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -2,16 +2,17 @@
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
+import { User } from "@supabase/supabase-js";
 
 export default function Account() {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState<null | User>(null);
     const router = useRouter();
 
     useEffect(() => {
         const getUser = async () => {
-            const { data: session } = await supabase.auth.getSession();
-            if (session?.user) {
-                setUser(session.user);
+            const { data } = await supabase.auth.getSession();
+            if (data.session) {
+                setUser(data?.session?.user);
             } else {
                 router.push("/register"); // Redirect to register if no user is logged in
             }
@@ -30,7 +31,6 @@ export default function Account() {
 
     return (
         <div>
-            {/* @ts-ignore */}
             <h1>Welcome, {user.email}</h1>
             <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded">
                 Logout
