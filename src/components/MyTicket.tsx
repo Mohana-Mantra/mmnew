f74865e5dd81ec46bb8a8287a15afb22a5df36a6
koushikamboru <koushic1980@gmail.com>
2024-09-20 01:37:47 +0530

    rscript t2

--- src/components/MyTicket.tsx
+++ src/components/MyTicket.tsx
@@ -123,14 +123,7 @@ const MyTicket = ({ user }: { user: User }) => {
             </button>
           ) : (
             <div className="razorpay-embed-btn" data-url="https://pages.razorpay.com/pl_OyuHRL0d2Kenle/view" data-text="Buy Your Pass" data-color="#528FF0" data-size="medium">
-              {/* Razorpay Payment Script */}
-              <script>
-                {`(function(){
-                  var d=document; var x=!d.getElementById('razorpay-embed-btn-js')
-                  if(x){ var s=d.createElement('script'); s.defer=!0;s.id='razorpay-embed-btn-js';
-                  s.src='https://cdn.razorpay.com/static/embed_btn/bundle.js';d.body.appendChild(s);} else{var rzp=window['_rzp_'];
-                  rzp && rzp.init && rzp.init()}})();`}
-              </script>
+              
             </div>
           )}
           {errorMessage && <p className="text-red-500 mt-4">{errorMessage}</p>}
