e5b96c6dcbe83acb894d955eed128207dda736d5
koushikamboru <koushic1980@gmail.com>
2024-09-23 10:09:17 +0530

    work , err in phno

--- src/components/MyTicket.tsx
+++ src/components/MyTicket.tsx
@@ -147,7 +147,7 @@ const MyTicket = ({ user }: { user: User }) => {
         resolve(true);
       } else {
         const script = document.createElement("script");
-        script.src = "https://pages.razorpay.com/pl_OyuHRL0d2Kenle/view";
+        script.src = "https://checkout.razorpay.com/v1/checkout.js";
         script.onload = () => resolve(true);
         script.onerror = () => resolve(false);
         document.body.appendChild(script);
