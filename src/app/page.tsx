a74202274c4dfb549485b7835092065901debc2d
koushikamboru <koushic1980@gmail.com>
2024-10-04 01:38:56 +0530

    rd coutdo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
             <ProShows />
             <Gallery />
             <Sponsers />
-            <StickyCountdown />
-            <Hackathon />
+           {/* <StickyCountdown />
+            <Hackathon />*/}
         </>
     );
 }
